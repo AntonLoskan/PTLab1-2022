168a17b75088ed36761e6dbb600f0ea8d3d9de83
AntonLoskan <loskan01@mail.ru>
2022-10-31 18:47:26 +0300

    Changed main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@
 import argparse
 import sys
 
+from pathlib import Path
 from CalcRating import CalcRating
+from CountSubjects import CountSubjects
 from TextDataReader import TextDataReader
+from TextDataReaderYaml import TextDataReaderYaml
+from pprint import pprint
 
 
 def get_path_from_arguments(args) -> str:
@@ -17,12 +21,29 @@ def get_path_from_arguments(args) -> str:
 def main():
     path = get_path_from_arguments(sys.argv[1:])
 
-    reader = TextDataReader()
-    students = reader.read(path)
-    print("Students: ", students)
+    fileExtension = Path(path).suffix
 
-    rating = CalcRating(students).calc()
-    print("Rating: ", rating)
+    if fileExtension == '.txt':
+        reader = TextDataReader()
+        students = reader.read(path)
+        print("Students: ", students)
+
+        rating = CalcRating(students).calc()
+        print("Rating: ", rating)
+    else:
+        reader = TextDataReaderYaml()
+        students = reader.read(path)
+        print("Students: ")
+        pprint(students)
+
+        rating = CountSubjects(students).calc()
+
+        for i in rating:
+            if rating.get(i) >= 3:
+                print("Student with 76 points in at least three disciplines: ", i)
+                break
+            else:
+                print("No students")
 
 
 if __name__ == "__main__":
